27e223839dbeb4ceae8f73b1fa385366b8e16e50
Marie Allpress <digitalinkfish@gmail.com>
2021-11-13 13:17:35 -0800

    contact page

--- screens/Contact.js
+++ screens/Contact.js
@@ -1,57 +1,51 @@
 import React from 'react';
-import { TextInput } from 'react-native-gesture-handler';
-/*import { ScrollView, Text } from 'react-native';
-import { Card } from 'react-native-elements';
-import * as Animatable from 'react-native-animatable'; */
-
-
+import { ScrollView, StyleSheet, Text } from 'react-native';
+import { Card, Button } from 'react-native-elements';
+import * as Animatable from 'react-native-animatable';
 
 const Contact = () => {
+  return (
+    <ScrollView>
+      <Animatable.View animation='fadeInDown' duration={2000} delay={1000}>
 
-    /*  return (
-          <ScrollView>
-              <Animatable.View animation='fadeInDown' duration={2000} delay={1000}>
-  
-                  <Card title="Contact Information" wrapperStyle={{ margin: 20 }}>
-                      <Text> Address: Anywhere, USA 12345 </Text>
-                      <Text Style={{ marginBottom: 10 }}> Phone: 123 456 7890 </Text>
-                      <Text> Email: info@domain.com </Text>
-                  </Card>
-  
-              </Animatable.View>
-          </ScrollView>
-      ); */
+        <Card title="Contact Information" wrapperStyle={{ margin: 20 }}>
+          <Text style={styles.headline} > FIND US </Text>
+          <Text> Address: Anywhere, USA 12345 </Text>
+          <Text> Phone: 123 456 7890 </Text>
+          <Text> Email: info@domain.com </Text>
+        </Card>
 
-    return (
-        <div>
-            <section class="colorlib-contact" data-section="contact">
-                <div className="col-md-7 col-md-push-1">
-                    <div className="row">
-                        <div className="col-md-10 col-md-offset-1 col-md-pull-1 animate-box" data-animate-effect="fadeInRight">
-                            <form action="">
-                                <div className="form-group">
-                                    <input type="text" className="form-control" placeholder="Name" />
-                                </div>
-                                <div className="form-group">
-                                    <input type="text" className="form-control" placeholder="Email" />
-                                </div>
-                                <div className="form-group">
-                                    <input type="text" className="form-control" placeholder="Subject" />
-                                </div>
-                                <div className="form-group">
-                                    <textarea name="" id="message" cols="30" rows="7" className="form-control" placeholder="Message"></textarea >
-                                </div>
-                                <div className="form-group">
-                                    <input type="submit" className="btn btn-primary btn-send-message" value="Send Message" />
-                                </div>
-                            </form>
-                        </div>
+        <Card title="Form" wrapperStyle={{ margin: 20 }}>
+          <Text>This will be the form</Text>
+          <Button
+                            title="Send Email"
+                            buttonStyle={{backgroundColor: '#5637DD', margin: 40}}
+                 
+                            onPress={() => this.sendMail()}
+                        />
+        </Card>
 
-                    </div>
-                </div>
-            </section>
-        </div>
-    );
-};
+      </Animatable.View>
+    </ScrollView>
+  );
+}
+
+const styles = StyleSheet.create({
+  center: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    textAlign: "center",
+  },
+  card:{
+
+  },
+    headline: {
+    textAlign: "center",
+    fontWeight: 'bold',
+    fontSize: 18,
+    marginBottom: 10,
+  },
+ });
 
 export default Contact;
